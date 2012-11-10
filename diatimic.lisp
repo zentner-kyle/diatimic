34ca380846064f30f0d59e4bb38a121b4db5907b
Andrew Chen <andrewtheannihilator@gmail.com>
2012-11-10 10:51:00 -0800

    added jquery, working sliding/counting

--- diatimic.lisp
+++ diatimic.lisp
@@ -29,12 +29,13 @@
 (defun add-file-to-path (file)
   (push
    (hunchentoot:create-static-file-dispatcher-and-handler
-         (concatenate 'string "/" file) (concatenate 'string *code-dir* file))
-        hunchentoot:*dispatch-table*))
+    (concatenate 'string "/" file) (concatenate 'string *code-dir* file))
+   hunchentoot:*dispatch-table*))
 
 (add-file-to-path "bootstrap.css")
 (add-file-to-path "mainpage.css")
 (add-file-to-path "bootstrap-responsive.css")
+(add-file-to-path "jquery-latest.js")
 
 (defun confirm-password (username password)
   (let ((real-password (gethash username *user-name-password-map*)))
@@ -49,6 +50,7 @@
              (:link :type "text/css" :href "/bootstrap.css" :rel "stylesheet")
              (:link :type "text/css" :href "/bootstrap-responsive.css" :rel "stylesheet")
              (:link :type "text/css" :href "/mainpage.css" :rel "stylesheet")
+             (:script :src "/jquery-latest.js")
              (:title ,title))
             ,@body)))
 
@@ -57,34 +59,71 @@
         (username (hunchentoot:post-parameter "username"))
         (password (hunchentoot:post-parameter "password")))
     (hunchentoot:no-cache)
-    (if (and username password)
-        (if (confirm-password username password)
-            (progn
-              (setf (hunchentoot:session-value 'username) username)
-              (std-html-page title
-                (:body
-                 (:center
-                  (:p (format t "Welcome, ~a." username))))))
-            (std-html-page :title "diatimic: Could not log in!"
-                           (:body (:center
-                                   (:p (format t "Sorry, could not log in as ~a" username))))))
-        (std-html-page :title title
-                       (:script :type "text/javascript"
-                                (cl-who:str (ps:ps
-                                       (defun login-callback ()
-                                         (alert "Logging in!")))))
-                       (:body (:center
-                               (:h1 "Welcome to diatimic, the graphing time tracker!"))
-                              (:p "Please login")
-                              (:p "Username")
-                              (:p (:form :method :post
-                                         (:input :type :text
-                                                 :name "username"
-                                                 :value "")))
-                              (:p "Password:")
-                              (:p (:form :method :post
-                                         (:input :type :password
-                                                 :name "password"
-                                                 :value "")))
-                              (:a :class "btn btn-primary btn-large" :style "padding:14px 0px; margin-bottom:0px;width: 50%;" :href "#" :onclick (ps:ps (login-callback))
-                                  "Login"))))))
+    (std-html-page :title title
+                   (:script :type "text/javascript"
+                            (cl-who:str
+                             (ps ((@ ($ document) ready)
+                                  (lambda ()
+                                        ;function starts here
+                                        ;(+ 1 2)
+                                    (progn
+                                      (setf time 0)
+                                      (setf stopped true)
+                                      ((@ window set-interval) periodic-task 1000)
+                                      ((@ ($ "div.second") append)
+                                       "<center class=added ><h1>0</h1></center>")
+                                      (+ 1 2)
+                                      nil
+
+
+                                      )))
+                                 (defun periodic-task ()
+                                   ((@ console log) "Periodic Task called")
+                                   (if (not stopped)
+                                       (progn
+                                         (setf time (+ 1 time))
+                                         ((@ ($ ".added") remove ))
+                                         ((@ ($ "div.second") append)
+                                          (concatenate 'string
+                                                       "<center class=added><h1>"
+                                                       time
+                                                       "</h1></center>"))
+                                         nil)))
+                                 ; pull the h1 up and down
+                                 (defun act-press ()
+                                   (progn
+                                     ((@
+                                       ((@
+                                         ((@ ($ "h1.diatimic") slide-up) 300)
+                                         delay) 1000)
+                                       slide-down) 300)
+                                     (setf stopped (not stopped))
+                                     nil
+                                     )
+                                   )
+
+                                 )
+                             )
+                            )
+                   (:body (:center
+                           (:h1 :class "diatimic" "Welcome to diatimic, the graphing time tracker!"))
+                          (:p "Please login")
+                          (:p "Username")
+                          (:p (:form :method :post
+                                     (:input :type :text
+                                             :name "username"
+                                             :value "")))
+                          (:p "Password:")
+                          (:p (:form :method :post
+                                     (:input :type :password
+                                             :name "password"
+                                             :value "")))
+                          (:a :class "btn btn-primary btn-large" :style "padding:14px 0px; margin-bottom:0px;width: 50%;" :href "#" :onclick (ps (login-callback))
+                              "Login")
+                          (:br )
+                          (:br )
+                          (:a :class "btn btn-primary btn-large" :style "padding:14px 0px; margin-bottom:0px;width: 50%;" :href "#" :onclick (ps (act-press))
+                              "Act")
+                          (:div :class "second")
+
+))))
