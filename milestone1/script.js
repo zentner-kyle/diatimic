0fbe3a28ad8b9efd91b70741f0dc1db55d1faf15
Andrew Chen <andrewtheannihilator@gmail.com>
2012-11-09 21:28:20 -0800

    playing with big button in milestone1 dir

--- milestone1/script.js
+++ milestone1/script.js
@@ -7,3 +7,28 @@ function periodicTask() {
   console.log("Period task called")
   $("div.second").slideUp(300).slideDown(300);
 }
+
+$(document).ready(function() {
+    //and then change this code so that your callback gets run
+    //when the button gets clicked instead of mine.
+    // **by the way, this is jQuery!
+  $("button").click(buttonActions);
+  $('#prettybutton').click(buttonActions);
+});
+
+$(document).ready(function(){
+
+  $("#ball_holder").everyTime(10, function(){
+    $("#ball_holder").animate({top:"184px"}, 400).animate({top:"0px"}, 370);
+  });
+
+  $("#start").click(function(){
+    $("#shoot").everyTime(10, function(){
+      $("#shoot").animate({left:"700px"}, 2500).animate({left:"10"}, 2500);
+    });
+  });
+  $("#stop").click(function(){
+    $("#shoot").stop(true).stopTime();
+  });
+
+});
